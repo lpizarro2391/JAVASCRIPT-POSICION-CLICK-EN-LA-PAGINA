537621fdc6eb5cd7896a3ab0f0bbc9bfd733b7c4
lpizarro2391 <lourdespizarro@gmail.com>
2020-01-15 19:53:33 -0600

    var calendario = document.getElementById('calendario'); calendario.addEventListener("click",function(event){   var mensaje = "Hiciste click en la posicion ->  X: "+event.clientX+" Y: "+event.clientY;   console.log(mensaje); })

---
+++ index.js
@@ -0,0 +1,5 @@
+var calendario = document.getElementById('calendario');
+calendario.addEventListener("click",function(event){
+  var mensaje = "Hiciste click en la posicion ->  X: "+event.clientX+" Y: "+event.clientY;
+  console.log(mensaje);
+})
